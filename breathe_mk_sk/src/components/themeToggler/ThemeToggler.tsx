e7bbdc7e140c4e9763102e6c0239f65059fad1c3
Nikoleta Stamenkovska-Stojmirovska <nikoleta.stamenkovska@yahoo.com>
2024-04-10 06:45:05 +0200

    before the changing of theme

--- breathe_mk_sk/src/components/themeToggler/ThemeToggler.tsx
+++ breathe_mk_sk/src/components/themeToggler/ThemeToggler.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import style from "./themeToggler.module.css";
 
 interface ThemeProps {
   theme: string;
@@ -7,8 +8,10 @@ interface ThemeProps {
 
 export const ThemeToggler: React.FC<ThemeProps> = ({ theme, toggleTheme }) => {
   return (
-    <label htmlFor="theme_toggler">
+    <label htmlFor="theme_toggler" className={style.theme_toggler_label}>
+      {theme === "light" ? "Light Mode" : "Dark Mode"}
       <input
+        className={style.theme_toggler}
         id="theme_toggler"
         type="checkbox"
         onChange={toggleTheme}
